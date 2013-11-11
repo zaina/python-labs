84a3d6df413dfc722b92a7cc2fbacf7d339c52bf
Zaina Afoulki <zenetcontact@gmail.com>
2013-11-11 10:20:53 -0600

    adding lab01 objectives

--- lab01.py
+++ lab01.py
@@ -1,5 +1,29 @@
  #!/usr/local/bin/python
 
+"""
+:mod:`lab01_pyreview` -- Python review
+=========================================
+
+LAB01 Learning Objective: Familiarization with argparse and parsing command line arguments.
+
+
+a. Review argparse module documentation
+
+b. Build an ArgumentParser object using the following parameters: 
+   description: "Patent database search engine"
+
+c. Add support for the following arguments and argument attributes: 
+   -a --author last first
+   -p --patent_num
+   -f --filing_date
+
+d. Run parse_args() to build arguments objects and print the Namespace
+
+e. Construct a generator called test_db_load() that returns a random function from a list of functions.
+   The list will support find_by_author(), find_by_patent_number(), and find_by_filing date()
+   Test your generator with stub functions
+"""
+
 import argparse
 from random import choice
 
